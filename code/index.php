a57e008f2f181aa6a322dc5952d106183b578b28
Noah-5555 <noah.d.geeler@gmail.com>
2021-10-12 11:16:44 +0200

    Updated Code
    Removed Config for CoinAPI

--- code/index.php
+++ code/index.php
@@ -1,7 +1,6 @@
 <?php
 
 $discord_config = json_decode(file_get_contents('discord_config.json'), JSON_OBJECT_AS_ARRAY);
-$coinapi_config = json_decode(file_get_contents('coinapi_config.json'), JSON_OBJECT_AS_ARRAY);
 
 include("discord.php");
 discordSendFirst();
