64d127afe4596911c5ffd51506cffc0a894ae7d1
Noah-5555 <noah.d.geeler@gmail.com>
2021-10-12 10:56:10 +0200

    First working Function with Config
    Added First Function and a Config File

--- code/discord.php
+++ code/discord.php
@@ -1,6 +1,30 @@
 <?php
 
-function discord()
+function discordSendFirst()
 {
     //Discord Webhook API
+    global $discord_config;
+
+    $url = $discord_config["webhook"];
+
+    $hookObject = json_encode([
+        "content" => $discord_config["message"]["content"],
+        "username" => $discord_config["username"],
+        "avatar_url" => $discord_config["avatar_url"],
+        "tts" => false
+    ], JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE );
+
+    $ch = curl_init();
+
+    curl_setopt_array( $ch, [
+        CURLOPT_URL => $url,
+        CURLOPT_POST => true,
+        CURLOPT_POSTFIELDS => $hookObject,
+        CURLOPT_HTTPHEADER => [
+            "Content-Type: application/json"
+        ]
+    ]);
+
+    // $response = curl_exec( $ch );
+    curl_close( $ch );
 }
--- code/index.php
+++ code/index.php
@@ -1,3 +1,7 @@
 <?php
 
-//Main Code here
+$discord_config = json_decode(file_get_contents('discord_config.json'), JSON_OBJECT_AS_ARRAY);
+$coinapi_config = json_decode(file_get_contents('coinapi_config.json'), JSON_OBJECT_AS_ARRAY);
+
+include("discord.php");
+discordSendFirst();
