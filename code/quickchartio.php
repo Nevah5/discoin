5c36e63ca675d6221ce28902723c690c628981b2
Noah-5555 <noah.d.geeler@gmail.com>
2021-10-15 09:04:59 +0200

    Update Quickchart
    Updated Colors of Chart

--- code/quickchartio.php
+++ code/quickchartio.php
@@ -12,8 +12,8 @@ function getChartURL()
         $data1 .= $key > 0 ? "," : "";
         $data1 .= (string) round($value["price"], 0);
     }
-    //edit template = https://quickchart.io/chart-maker/edit/zm-a9bdc7f7-1bfd-498e-b866-d14f77c9c541
-    $chartUrl = 'https://quickchart.io/chart/render/zm-4a8223aa-e6b9-4c33-8e29-5870d73c533f?data1=' . $data1;
+    //edit template = https://quickchart.io/chart-maker/edit/zm-41f2fcb2-0313-4f6e-9f25-f671e597d3e0
+    $chartUrl = 'https://quickchart.io/chart/render/zm-a2200d6b-21df-4184-8b70-019ff7b8d839?data1=' . $data1;
 
     return $chartUrl;
 }
