31383e0b4af2acf5fa419b4699c0adf95969af8a
Noah-5555 <noah.d.geeler@gmail.com>
2021-10-19 02:04:12 +0200

    Update Chart
    Made the Chart more "viewable"

--- code/quickchartio.php
+++ code/quickchartio.php
@@ -10,8 +10,8 @@ function getChartURL()
         $data1 .= $key > 0 ? "," : "";
         $data1 .= (string) round($value["price"], 0);
     }
-    //edit template = https://quickchart.io/chart-maker/edit/zm-41f2fcb2-0313-4f6e-9f25-f671e597d3e0
-    $chartUrl = 'https://quickchart.io/chart/render/zm-a2200d6b-21df-4184-8b70-019ff7b8d839?data1=' . $data1;
+    //edit template = https://quickchart.io/chart-maker/edit/zm-c5a44b72-55aa-48d2-b9b3-4fe61dfd50de
+    $chartUrl = 'https://quickchart.io/chart/render/zm-1b37d2a6-7311-4757-8ba3-ff93a126c379?data1=' . $data1;
 
     return $chartUrl;
 }
